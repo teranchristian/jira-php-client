9c9ee1e5d719278bd9fdffe654ab690b46e0a38f
teranchristian <chris.teran@gmail.com>
2017-04-03 20:14:55 +1000

    Add quick config file

--- cli/config.php
+++ cli/config.php
@@ -4,4 +4,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 define('JIRACLI_PATH', __DIR__); 
 
 use Jira\Client;
+
+$configFile = __DIR__.'/../config/config.ini';
+
+if (!file_exists($configFile)) {
+    die("Unknown config file $configFile\n");
+}
+
+$config = parse_ini_file($configFile, true);
+$jiraConfig = $config['Jira'];
+$hostJira = $jiraConfig['host'];
+$user = $jiraConfig['user'];
+$password = $jiraConfig['password'];
+
 $client = new Client($hostJira, $user , $password);
